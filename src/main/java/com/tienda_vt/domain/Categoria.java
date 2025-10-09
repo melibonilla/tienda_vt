d2b5746498ecba03aa4f52f279698f0e613af9c8
melibonilla <136756568+melibonilla@users.noreply.github.com>
2025-10-09 02:55:12 -0600

    Semana 3: DB

---
+++ src/main/java/com/tienda_vt/domain/Categoria.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda_vt.domain;
+import jakarta.persistence.*;
+import java.io.Serializable;
+//import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/**
+ *
+ * @author melissa
+ */
+@Data
+@Entity
+@Table (name ="categoria")
+public class Categoria implements Serializable {
+    private static final long serialVersionUID=1L;
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer idCategoria;
+    
+    @Column (unique=true, nullable=false, length=50)
+    //@NotNull
+    //@Size(max=50)
+    private String descripcion;
+    
+    
+    //@Size (max=1024)
+    private String rutaImagen;
+    private boolean activo;
+    
+}
---
+++ src/main/java/com/tienda_vt/service/CategoriaService.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda_vt.service;
+
+/**
+ *
+ * @author melissa
+ */
+
+
+import com.tienda_vt.domain.Categoria;
+import com.tienda_vt.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    
+    @Transactional (readOnly=true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        }
+        return categoriaRepository.findAll();
+        
+    }
+}
